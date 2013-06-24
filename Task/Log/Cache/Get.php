689fe81cb8bbebabbd2d667c2b3228b94cde98b8
srcspider <source.spider@gmail.com>
2013-06-24 04:08:16 +0300

    formatting++

--- Task/Log/Cache/Get.php
+++ Task/Log/Cache/Get.php
@@ -2,7 +2,7 @@
 
 /**
  * @package    mjolnir
- * @category   Cascading File System
+ * @category   Profile
  * @author     Ibidem Team
  * @copyright  (c) 2013, Ibidem Team
  * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
