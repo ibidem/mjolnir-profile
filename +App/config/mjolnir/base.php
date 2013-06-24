8ae04be7ed80d59ea8a6928ba7444eb69082f8fe
srcspider <source.spider@gmail.com>
2013-06-24 03:43:32 +0300

    short sql logging

---
+++ +App/config/mjolnir/base.php
@@ -0,0 +1,11 @@
+<?php return array
+	(
+		'logging' => array
+			(
+				// the short.sql.log logs a short version of database queries,
+				// a SELECT will get logged as fields, FROM, LIMIT, OFFSET, and
+				// WHERE (ie. ommiting JOINs, etc)
+				'short.sql.log' => false,
+			),
+	
+	); # config
--- SQLStatement.php
+++ SQLStatement.php
@@ -11,25 +11,183 @@ class SQLStatement extends next\SQLStatement
 {
 	function run()
 	{
-		$dbtoken = \app\Benchmark::token("database", 'Application');
-		
-		$idpos = \strpos($this->query, '--');
-		$identity = \substr($this->query, $idpos + 2);
-		$token = \app\Benchmark::token("<span title=\"{$this->query}\">$identity</span>", 'Database Queries');
-		
-		try
+		$baseconfig = \app\CFS::config('mjolnir/base');
+
+		if ($baseconfig['development'])
 		{
-			$result = parent::run();
-			\app\Benchmark::stop($token);
-			\app\Benchmark::stop($dbtoken);
-			return $result;
+			$dbtoken = \app\Benchmark::token("database", 'Application');
+
+			$idpos = \strpos($this->query, '--');
+			$identity = \substr($this->query, $idpos + 2);
+			$token = \app\Benchmark::token("<span title=\"{$this->query}\">$identity</span>", 'Database Queries');
+
+			if ($baseconfig['logging']['short.log'] && $baseconfig['logging']['short.sql.log'])
+			{
+				// extract sql query
+				$thequery = \preg_replace('#[ ]{2,}#', ' ', \preg_replace('#[\t\n\r]#', ' ', \trim($this->query)));
+				\preg_match('#^(SELECT|UPDATE|INSERT) #', $thequery, $match);
+
+				// did we get match?
+				if (isset($match[1]))
+				{
+					$querytype = \strtoupper($match[1]);
+				}
+				else # no match; MISC query
+				{
+					$querytype = 'Misc';
+				}
+
+				if ($querytype == 'SELECT')
+				{
+					$shortquery = '';
+
+					// extra fields
+					if (\preg_match('#^SELECT (.*) FROM#', $thequery, $match))
+					{
+						$shortquery .= \str_replace('`', '', $match[1]);
+
+						// extract FROM
+						if (\preg_match('#FROM ([^ ]*)( |$)#', $thequery, $match))
+						{
+							$shortquery .= ' FROM '.\str_replace('`', '', $match[1]);
+
+							// try to extract LIMIT
+							if (\preg_match('#LIMIT ([0-9]+)( |$)#', $thequery, $match))
+							{
+								$shortquery .= ' LIMIT '.$match[1];
+							}
+
+							// try to extract OFFSET
+							if (\preg_match('#OFFSET ([0-9]+)( |$)#', $thequery, $match))
+							{
+								$shortquery .= ' OFFSET '.$match[1];
+							}
+
+							// extract where clause
+							if (\preg_match('#WHERE (.*)#', $thequery, $match))
+							{
+								$shortquery .= ' WHERE '.\trim(\preg_replace('#--(.*)$#', '', \str_replace('`', '', $match[1])));
+							}
+
+							$logpath = \app\Env::key('etc.path').'logs/';
+							$time = \date('Y-m-d H:i:s');
+
+							// write short query
+							$message = \sprintf
+								(
+									" %s --- %6s %s",
+									$time,
+									$querytype,
+									$shortquery
+								);
+
+							\mjolnir\append_to_file
+								(
+									$logpath,
+									'short.sql.log',
+									PHP_EOL.$message
+								);
+						}
+					}
+				}
+				else if ($querytype == 'UPDATE')
+				{
+					$shortquery = '';
+
+					// extra fields
+					if (\preg_match('#^UPDATE ([^ ]+)#', $thequery, $match))
+					{
+						$shortquery .= \str_replace('`', '', $match[1]);
+
+						$logpath = \app\Env::key('etc.path').'logs/';
+						$time = \date('Y-m-d H:i:s');
+
+						// write short query
+						$message = \sprintf
+							(
+								" %s --- %6s %s",
+								$time,
+								$querytype,
+								$shortquery
+							);
+
+						\mjolnir\append_to_file
+							(
+								$logpath,
+								'short.sql.log',
+								PHP_EOL.$message
+							);
+					}
+				}
+				else if ($querytype == 'INSERT')
+				{
+					$shortquery = '';
+
+					// extra fields
+					if (\preg_match('#^INSERT INTO ([^ ]+)#', $thequery, $match))
+					{
+						$shortquery .= \str_replace('`', '', $match[1]);
+
+						$logpath = \app\Env::key('etc.path').'logs/';
+						$time = \date('Y-m-d H:i:s');
+
+						// write short query
+						$message = \sprintf
+							(
+								" %s --- %6s %s",
+								$time,
+								$querytype,
+								$shortquery
+							);
+
+						\mjolnir\append_to_file
+							(
+								$logpath,
+								'short.sql.log',
+								PHP_EOL.$message
+							);
+					}
+				}
+				else # misc
+				{
+					$logpath = \app\Env::key('etc.path').'logs/';
+						$time = \date('Y-m-d H:i:s');
+
+						// write short query
+						$message = \sprintf
+							(
+								" %s --- %s",
+								$time,
+								'Misc Query'
+							);
+
+						\mjolnir\append_to_file
+							(
+								$logpath,
+								'short.sql.log',
+								PHP_EOL.$message
+							);
+				}
+			}
+
+			try
+			{
+				$result = parent::run();
+				\app\Benchmark::stop($token);
+				\app\Benchmark::stop($dbtoken);
+				return $result;
+			}
+			catch (\Exception $e)
+			{
+				\app\Benchmark::stop($token);
+				\app\Benchmark::stop($dbtoken);
+				throw $e;
+			}
 		}
-		catch (\Exception $e)
+		else # non-development
 		{
-			\app\Benchmark::stop($token);
-			\app\Benchmark::stop($dbtoken);
-			throw $e;
+			return parent::run();
 		}
 	}
-	
+
 } # class
